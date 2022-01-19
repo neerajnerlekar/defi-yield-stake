da496e3064141d365e75943d225403d90e01f1e1
neerajnerlekar <neeraj.nerlekar@gmail.com>
2022-01-19 16:22:17 -0700

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,73 @@
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import DappToken, TokenFarm, config, network
+from web3 import Web3
+import yaml
+import json, os, shutil
+
+KEPT_BALANCE = Web3.toWei(100, "ether")
+
+
+def deploy_token_farm_and_dapp_token(front_end_update=False):
+    account = get_account()
+    dapp_token = DappToken.deploy({"from": account})
+    print(dapp_token.totalSupply())
+    print(dapp_token.symbol())
+    print(dapp_token.name())
+
+    token_farm = TokenFarm.deploy(
+        dapp_token.address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()]["verify"],
+    )
+    tx = dapp_token.transfer(
+        token_farm.address, dapp_token.totalSupply() - KEPT_BALANCE, {"from": account}
+    )
+    tx.wait(1)
+
+    # tokens to stake -> dapp_token, weth_token, fau_token/dai. Now lets get the address of these token. Adding them in brownie-config.
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    dict_of_allowed_tokens = {
+        dapp_token: get_contract("dai_usd_price_feed"),
+        fau_token: get_contract("dai_usd_price_feed"),
+        weth_token: get_contract("eth_usd_price_feed"),
+    }
+
+    add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+    if update_front_end:
+        update_front_end()
+    return token_farm, dapp_token
+
+
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    for token in dict_of_allowed_tokens:
+        add_tx = token_farm.addAllowedTokens(token.address, {"from": account})
+        add_tx.wait(1)
+        set_tx = token_farm.setPriceFeedContract(
+            token.address, dict_of_allowed_tokens[token], {"from": account}
+        )
+        set_tx.wait(1)
+    return token_farm
+
+
+def update_front_end():
+    # Sending the front end our config in JSON format, as typescript does not work with yaml files.
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader= yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print("Front end updated!")
+    # This gives the token addresses for fau and weth token but not the dapp token. So need to send the build folder to front end also. 
+    copy_folders_to_front_end("./build", "./front_end/src/chain-info")
+
+def copy_folders_to_front_end(src, dest):
+    # this check if the folder already exist
+    if os.path.exists(dest):
+        #completely removes the mentioned folder
+        shutil.rmtree(dest)
+    # copies the mentioned folder
+    shutil.copytree(src, dest)
+
+
+def main():
+    deploy_token_farm_and_dapp_token(front_end_update=True)
